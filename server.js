f1bbf3c89058b6424d092ca28e25cc6a95f40e91
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-30 17:52:33 -0400

    Add Server.js File
    Comment
    
    Added route imports from the config file
    Added an instance of the server for export

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-// const express = require('express');
-// const cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
-// const configureRoutes = require('./config/routes');
+const configureRoutes = require('./config/routes');
 
-// const server = express();
-// const corsOptions = {
-//   // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
-//   // ensure that your client's URL/Port can achieve a Handshake
-//   // then pass this object to the cors() function
-// };
+const server = express();
+const corsOptions = {
+  // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
+  // ensure that your client's URL/Port can achieve a Handshake
+  // then pass this object to the cors() function
+};
 
-// server.use(express.json());
-// server.use(cors());
+server.use(express.json());
+server.use(cors());
 
-// configureRoutes(server);
+configureRoutes(server);
 
-// module.exports = {
-//   server,
-// };
+module.exports = {
+  server,
+};
