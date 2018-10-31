8a47c850802310dee4f72412ac92b5a07ad69c8e
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-31 09:56:25 -0400

    Add/Load Server Object on Index.js
    Comment
    
    Load the server object on the index.js file from server.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-//Some of the code came from dotenv package and I do not have 
-//const { server } = require('./server.js');
-const express = require('express');
+const { server } = require('./server.js');
 
-const server = express();
 
-//const port = process.env.PORT || 3300;
-const port = 3300;
+const port = process.env.PORT || 3300;
+//const port = 3300;
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
 });
